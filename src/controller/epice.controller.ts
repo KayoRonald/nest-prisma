e582a63f872ecd308182594a4be3b9b8e84e6f30
KayoRonald <kayoronald4321@gmail.com>
2022-05-24 00:33:23 +0000

    feat: CRUD

--- src/controller/epice.controller.ts
+++ src/controller/epice.controller.ts
@@ -1,31 +1,55 @@
-import { Controller, Get, Res } from '@nestjs/common';
-import { Response } from 'express'
+import {
+  Controller,
+  Get,
+  Res,
+  Delete,
+  Put,
+  Post,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
 import { EpiceService } from 'src/service/app.service';
 
 @Controller()
 export class EpiceController {
-  constructor(private readonly EpiceService: EpiceService) { }
+  constructor(private readonly EpiceService: EpiceService) {}
   @Get('epice')
   async list() {
-    return this.EpiceService.getListUsers()
+    return this.EpiceService.getListUsers();
   }
 
-  @Get('updateName')
-  async updateName(@Res() response: Response) {
-    var name = 'jão Paulo Silva'
-    var email = 'joaopaulo@gmail.com'
-    await this.EpiceService.updateName(name, email)
+  @Post('epice/:curso')
+  async createUsers(
+    @Res() response: Response,
+    @Param() params,
+    @Body() input: Prisma.UsersCreateInput,
+  ) {
+    console.log(input);
+    await this.EpiceService.createUsers(input, params.curso);
     return response.status(201).json({
-      message: 'Nome atualizado com sucesso!'
-    })
+      message: 'Cadastro feito com sucesso!',
+    });
   }
-  
-  @Get('delete')
-  async deleteWhere(@Res() response: Response) {
-    var email = 'kayoronald@gmail.com'
-    await this.EpiceService.deleteWhere(email)
+
+  @Put('epice/:email')
+  async updateName(
+    @Res() response: Response,
+    @Param() params,
+    @Body('name') name: string,
+  ) {
+    await this.EpiceService.updateName(name, params.email);
     return response.status(201).json({
-      message: 'Deletado com sucesso!'
-    })
+      message: 'Nome atualizado com sucesso!',
+    });
+  }
+
+  @Delete('epice/:email')
+  async deleteWhere(@Res() response: Response, @Param() params) {
+    await this.EpiceService.deleteWhere(params.email);
+    return response.status(201).json({
+      message: 'Deletado com sucesso!',
+    });
   }
 }
