7101a38ed55e9604e6af858eab2b58d2a6b2fbce
KayoRonald <kayoronald4321@gmail.com>
2022-05-24 00:42:03 +0000

    fix: remove code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { EpiceModule } from './modules/app.module';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+
 async function bootstrap() {
   const app = await NestFactory.create(EpiceModule);
   await app.listen(3333,()=>{
